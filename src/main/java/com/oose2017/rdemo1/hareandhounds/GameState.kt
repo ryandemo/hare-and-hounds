98bc0d35780cda9a3052a2c0ed297bc309f1bdfa
Ryan Demo <ryan.demo@me.com>
2017-09-07 15:46:55 -0400

    Wrote GameState, Piece enums and PiecePosition data class

--- src/main/java/com/oose2017/rdemo1/hareandhounds/GameState.kt
+++ src/main/java/com/oose2017/rdemo1/hareandhounds/GameState.kt
@@ -1,2 +1,27 @@
 package com.oose2017.rdemo1.hareandhounds
 
+enum class GameState {
+    WAITING_FOR_SECOND_PLAYER, TURN_HARE, TURN_HOUND, WIN_HARE_BY_ESCAPE, WIN_HARE_BY_STALLING, WIN_HOUND
+}
+
+fun PieceFromPlayerId(id: Int): Piece? = when(id) {
+    0 -> Piece.HARE
+    1 -> Piece.HOUND
+    else -> null
+}
+
+enum class Piece {
+    HARE, HOUND;
+
+    fun asPlayerId(): String = when(this) {
+        HARE -> "0"
+        HOUND -> "1"
+    }
+
+    fun opponent(): Piece = when(this) {
+        HARE -> HOUND
+        HOUND -> HARE
+    }
+}
+
+data class PiecePosition(val piece: Piece, var x: Int, var y: Int)
