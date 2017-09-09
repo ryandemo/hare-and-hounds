997192fdd9266842a3e1a1e5e7f132ff3901f28e
Ryan Demo <ryan.demo@me.com>
2017-09-09 17:47:20 -0400

    Configured Bootstrap

--- src/main/java/com/oose2017/rdemo1/hareandhounds/Bootstrap.kt
+++ src/main/java/com/oose2017/rdemo1/hareandhounds/Bootstrap.kt
@@ -19,49 +19,18 @@ object Bootstrap {
     @Throws(Exception::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        //Check if the database file exists in the current directory. Abort if not
-        val dataSource = configureDataSource()
-        if (dataSource == null) {
-            System.out.printf("Could not find todo.db in the current directory (%s). Terminating\n",
-                    Paths.get(".").toAbsolutePath().normalize())
-            System.exit(1)
-        }
 
-        //Specify the IP address and Port at which the server should be run
+        // Specify the IP address and Port at which the server should be run
         ipAddress(IP_ADDRESS)
         port(PORT)
 
         //Specify the sub-directory from which to serve static resources (like html and css)
         staticFileLocation("/public")
 
-        //Create the model instance and then configure and start the web service
-        try {
-            val model = TodoService(dataSource)
-            TodoController(model)
-        } catch (ex: TodoService.TodoServiceException) {
-            logger.error("Failed to create a GameService instance. Aborting")
-        }
+        // Create a new game data object
+        val gameDAO = GameDAO()
+        val gameService = GameService(gameDAO)
+        GameController(gameService)
 
-    }
-
-    /**
-     * Check if the database file exists in the current directory. If it does
-     * create a DataSource instance for the file and return it.
-     * @return javax.sql.DataSource corresponding to the todo database
-     */
-    private fun configureDataSource(): DataSource? {
-        val dbPath = Paths.get(".", "harehound.db")
-        if (!Files.exists(dbPath)) {
-            try {
-                Files.createFile(dbPath)
-            } catch (ex: java.io.IOException) {
-                logger.error("Failed to create harehound.db file in current directory. Aborting")
-                return null
-            }
-        }
-
-        val dataSource = SQLiteDataSource()
-        dataSource.url = "jdbc:sqlite:todo.db"
-        return dataSource
     }
 }
